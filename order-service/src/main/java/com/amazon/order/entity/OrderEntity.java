3a4039e79c4f76007061e13c574720285b1adfab
Anujkumar0708 <anujkumarkush9839@gmail.com>
2026-03-10 17:34:02 +0530

    change to configs

--- order-service/src/main/java/com/amazon/order/entity/OrderEntity.java
+++ order-service/src/main/java/com/amazon/order/entity/OrderEntity.java
@@ -1,12 +1,18 @@
 package com.amazon.order.entity;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "orders")
 public class OrderEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int orderId;
 	private String status;
 	private String itemName;
